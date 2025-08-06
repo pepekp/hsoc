91c2b2d0bf4c84db1e3e10a788ad4457b8a2a072
pepekp <pepe@debitop.home.lab>
2025-08-06 14:56:54 +0300

    update files

--- sysloging/syslog_srv_v1.py
+++ sysloging/syslog_srv_v1.py
@@ -1,72 +1,19 @@
-import socketserver
-import logging
-
+"""
+Syslog server 
+"""
 import logging
 import logging.handlers
 import socketserver
-import datetime
-import time
-import os
-import glob
-from datetime import datetime, timedelta
 from venv import logger
 from directory_path import syslog_dir_path
 
-#syslog_dir_name = '/home/pepe/PycharmProjects/siem/sysloging/syslogs/'
-
-# def find_last_created_file():
-#     files_path = os.path.join(syslog_dir_name, '*')
-#     files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
-#     file_name = files[0].split('/')
-#     last_file_in_dir = file_name[-1]
-#     file_name_time = os.path.getctime(syslog_dir_name + file_name[-1])
-#     # Convert Unix time
-#     file_time = datetime.fromtimestamp(file_name_time)
-#     last_created_file_time = file_time.strftime("%a_%b_%d_%H_%M_%S_%Y")
-#     # Convert str to date.time
-#     last_created_file_time_dt = datetime.strptime(last_created_file_time, "%a_%b_%d_%H_%M_%S_%Y")
-#     print('file name and file create time ')
-#     print(last_created_file_time_dt)
-#
-#     return last_created_file_time, last_file_in_dir
-#     # print(files[0])
-#     # print(file_name[-1])
-# last_created_file_and_time = find_last_created_file()
-# print(f'Function return {last_created_file_and_time[0]} {last_created_file_and_time[1]}')
-# # print(type(last_created_file_and_time[0]))
-# # print(type(last_created_file_and_time[1]))
-# print('==================================================================================')
-# def generate_syslog_file():
-#     t = time.localtime()
-#     current_time = time.strftime("%a_%b_%d_%H_%M_%S_%Y", t)
-#     time_now = datetime.now()
-#     # Define how old should be the last created file
-#     time_delta = timedelta(minutes=5)
-#     time_ago = time_now - time_delta
-#     create_time_ago = time_ago.strftime("%a_%b_%d_%H_%M_%S_%Y")
-#     create_time_ago_datetime = datetime.strptime(create_time_ago, "%a_%b_%d_%H_%M_%S_%Y")
-#     last_created_file_datetime = datetime.strptime(last_created_file_and_time[0], "%a_%b_%d_%H_%M_%S_%Y")
-#     # Generate datetime var based on file create time
-#     if create_time_ago_datetime > last_created_file_datetime:
-#         t = time.localtime()
-#         current_time = time.strftime("%a_%b_%d_%H_%M_%S_%Y", t)
-#         file_name = current_time + '.log'
-#         with open(syslog_dir_name + '/' + file_name, 'w') as log_file:
-#             log_file.close()
-#         print(f'New log file is has been created {file_name}')
-#     else:
-#         print('use last created file')
-#         file_name = last_created_file_and_time[1]
-#     return file_name
-# log_file_name = generate_syslog_file()
-# print(f'generate syslog file {log_file_name}')
 
 # Configuration
 HOST, PORT = "0.0.0.0", 1514  # Listen on all interfaces, standard syslog port
 LOG_FILE = "syslog.log"
 
 # Set up logging to a file
-#rotator = logging.FileHandler(log_file_name, mode='a', encoding=None, errors=None)
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s',
                     filename=f'{syslog_dir_path}/{LOG_FILE}', filemode='a')
 
@@ -88,10 +35,9 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
     print('server start')
     def handle(self) -> None:
         data = bytes.decode(self.request[0].strip())  # Decode the received bytes
-        #print(type(data))
+
         socket = self.request[1]  # The socket, not used here but can be useful
         print(f"{self.client_address[0]}: {data}")  # Print to console
-        #logging.info(data)  # Log to file
         logger.info(data)
         print('log rotator')
 
@@ -110,9 +56,5 @@ def main():
 
 
 if __name__ == "__main__":
-    #find_last_created_file()
-    #generate_syslog_file()
-    #log_file_name = generate_syslog_file()
-    #print(f'generate syslog file {log_file_name}')
     main()
 
